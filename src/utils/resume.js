556ded9a0fe6ea496eefbd30d6b11f47a784f811
刘信林 <liuxinlin@xinchanedu.com>
2019-09-01 17:46:27 +0800

    first commit

---
+++ src/utils/resume.js
@@ -0,0 +1,321 @@
+export default {
+    'basics': {
+        'position': 'Web 前端工程师',
+        'name': '刘信林',
+        'gender': '男',
+        'age': '27',
+        'worklife': '5',
+        'label': '嘻嘻哈哈奋斗的小青年',
+        'picture': 'https://www.xinlinyun.com/lxl/imgs/avatar.jpg',
+        'email': 'lxlin0172@163.com',
+        'phone': '18500301496',
+        'website': 'https://github.com/lxlyjss',
+        'summary': [
+            '个人主攻 Web 前端和 NodeJS 开发，5年+前端开发经验，参与或独立开发过多个核心项目，项目类型涉及：网站应用、教育服务、企业管理、微信小程序、SPA 应用（PC/Moblie）等等',
+            '喜欢关注 Web 前端相关新技术（NodeJs、Vue、微信小程序...），对于新技术有很强的学习兴趣',
+            '热爱前端这份工作，对工作热情负责。'
+        ],
+        'location': '北京市朝阳区',
+        'profiles': [
+            {
+                'network': 'github',
+                'username': 'lxlyjss',
+                'url': 'https://github.com/lxlyjss'
+            }
+        ]
+    },
+    'work': [
+        {
+            'company': '信产教育科技有限公司',
+            'position': 'Web 前端工程师',
+            'website': 'http://www.aiqbg.com',
+            'startDate': '2018-10',
+            'endDate': '至今',
+            'summary': '信产教育科技有限公司',
+            'highlights': [
+                '主要负责AI情报官C端微信小程序开发和维护',
+                '参与AI情报官B端企业微信第三方H5应用，公共组件开发',
+                '参与AI情报官PC端saas系统开发',
+                '参与AI情报官整体项目需求研讨及制定',
+                '前端公共组件'
+            ]
+        },
+        {
+            'company': '北京福悦天成文化传播有限公司',
+            'position': 'Web 前端工程师',
+            'website': 'http://www.loveartcn.com',
+            'startDate': '2018-01',
+            'endDate': '2018-09',
+            'summary': '北京福悦天成文化传播有限公司',
+            'highlights': [
+                '根据产品需求文档，进行 Web 前端开发，前端主体框架选型，搭建基础框架',
+                '根据设计提供的设计稿编写静态页面',
+                '负责恋爱艺术微信小程序及crm系统开发与维护',
+                '负责配合后台开发人员，完成页面的交互及接口对接',
+            ]
+        },
+        {
+            'company': '北京骑二无比网络科技有限公司',
+            'position': 'Web 前端工程师',
+            'website': 'http://www.72bike.com',
+            'startDate': '2017-03',
+            'endDate': '2018-01',
+            'summary': '北京骑二无比网络科技有限公司',
+            'highlights': [
+                'Photoshop 对设计图进行切片',
+                '根据设计提供的设计稿编写静态页面',
+                '负责配合后台开发人员，完成页面的交互及接口对接',
+                '负责网站功能模块及后台管理系统的设计'
+            ]
+        },
+        {
+            'company': '上海唯杰信息技术有限公司',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2014-09',
+            'endDate': '2016-12',
+            'summary': '上海唯杰信息技术有限公司',
+            'highlights': [
+                'Photoshop 对设计图进行切片',
+                'HTML5 + CSS3 编写网站页面，并保证浏览器的兼容性',
+                '负责配合后台开发人员，完成页面的交互及接口对接',
+                '负责电脑端及移动端 Web 项目的前端开发'
+            ]
+        }
+    ],
+    'project': [
+        {
+            'organization': '三体云动商家小程序第三方模板',
+            'position': 'Web 前端工程师',
+            'website': 'https://www.styd.cn',
+            'startDate': '2019-06',
+            'endDate': '2019-07',
+            'summary': 'gulp + vant-ui + 微信小程序',
+            'highlights': [
+                '这是2019年6月中旬出差到上海公司总部开发的一款微信小程序模板，为健身行业的商家提供模板代码，功能包括健身品牌介绍，门店介绍，教练介绍和咨询，约课，购卡购课，健身数据，营销活动等功能。',
+                '由于是总公司新的项目，只有三个人参与前端开发，架构由另一个人搭建完成，大部分的公共组件都是由我进行封装，整个项目持续一个月之后上线第一版。',
+                '根据 UI 提供的设计稿，完美还原页面及其交互效果，并且通过小程序 rpx 设计方案兼容多尺寸移动设备。',
+                '项目经验收，无重大漏洞，正式上线后，用户体验良好，功能完善。'
+            ]
+        },
+        {
+            'organization': 'AI情报官微信小程序第三方模板',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-10',
+            'endDate': '2019-08',
+            'summary': 'gulp + vant-ui + 微信小程序',
+            'highlights': [
+                '利用微信开放平台创建第三方应用，使用托管商家小程序的方式为商家开发小程序模板，集成开发，审核，发布一体化流程',
+                '实现分享名片，保存名片海报，展示名片，微官网展示，分享等功能；',
+                '实现商城，下单，支付，拼团，砍价，幸运大转盘等营销功能。',
+                '使用gulp自动化构建打包微信小程序原生代码，利用微信开发者工具命令行集成打包上传再到发布自动化功能。',
+                '整个项目前期由我一人搭建并开发，后期总共三人共同协同开发'
+            ]
+        },
+        {
+            'organization': 'AI情报官员工端H5',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-10',
+            'endDate': '2018-03',
+            'summary': 'Vue + Vue-Router + Vuex + Axios + Wepack + ES6 + Mint-UI + H5应用',
+            'highlights': [
+                '实习聊天咨询功能，员工管理，c端用户动作捕获，员工名片编辑，用户行为分析等功能',
+                '使用vue全家桶结合mint-ui开发的一款h5应用，入口是企业微信第三方应用，并集成了微信jssdk，利用jssdk提供的微信的相关功能开发项目',
+                '我负责项目初始化架构搭建以及聊天咨询模块，后期维护以及优化代码结构等'
+            ]
+        },
+        {
+            'organization': 'AI情报官saas系统',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-10',
+            'endDate': '2019-03',
+            'summary': 'Vue + Vue-Router + Vuex + Axios + Wepack + Element-UI + ES6',
+            'highlights': [
+                '团队三人协同开发一款供应链 SaaS 软件，主要功能包括门店管理、员工管理，商品管理，营销活动管理，订单管理等。',
+                '我在此项目中担任 Web 前端工程师，负责参与项目的前端项目架构，公共组件封装等。',
+                '项目基于 Vue + Vue-Router + Vuex + Axios + Wepack + Element-UI + ES6 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新。'
+            ]
+        },
+        {
+            'organization': '恋爱艺术crm后台管理系统',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-01',
+            'endDate': '2018-08',
+            'summary': 'Vue + Vue-Router + Vuex + Axios + Wepack + Element-UI + ES6',
+            'highlights': [
+                '这是一款管理c端app和微信小程序数据的后台管理系统，使用vue全家桶以及element-ui框架搭建',
+                '主要功能包括课程购买观看，订单管理，权限管理，直播管理，充值管理，文章管理等',
+                '由于公司只有我一个前端，所以项目由我一个人负责搭建，开发和维护'
+            ]
+        },
+        {
+            'organization': '恋爱艺术官网',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-02',
+            'endDate': '2018-06',
+            'summary': 'Vue + Vue-Router + Vuex + Axios + Wepack + Element-UI + ES6',
+            'highlights': [
+                '这是恋爱艺术官方网站，主要为用户浏览文章，点赞，评论使用，以及用户自己发布文章等功能',
+                '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+                '项目基于vue全家桶开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新；',
+                '项目上线后，前端性能表现良好，测试结果也没有任何异常，用户活跃数量维持在10万左右，总用户数量超过50万。'
+            ]
+        },
+        {
+            'organization': '恋爱艺术微信小程序',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018-06',
+            'endDate': '2018-09',
+            'summary': '微信小程序',
+            'highlights': [
+                '主要为用户提供在线观看视频，音频学习教程，浏览文章，分享，点赞，评论以及购买课程的功能。',
+                '作为团队中的核心程序员，由于第一次接触微信小程序，我需要熟悉小程序的api文档，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+                '项目上线后，存在一些小问题，大都是由于微信api文档介绍不完善，以及不同机型存在的一些差异bug等，后来在维护的过程中陆续得到修复。'
+            ]
+        },
+        {
+            'organization': '骑二无比赛事报名系统',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2017-09',
+            'endDate': '2017-11',
+            'summary': 'jQuery + html5 + css3 + 原生js',
+            'highlights': [
+                '这是骑二无比举办赛事时为用户提供的线上报名系统，节省了了线下报名的人力成本。',
+                '作为团队中的核心前端程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+                '项目时运行在微信网页中，需要调用微信的相关接口，登录，支付，及分享等',
+                '项目上线之后就为公司赛事报名提供了很大的方便，公司领导非常满意。'
+            ]
+        },
+        {
+            'organization': '捷安特数据展示系统',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2017-06',
+            'endDate': '2017-09',
+            'summary': 'jQuery + html5 + css3 + 原生js + echarts',
+            'highlights': [
+                '该项目为公司对接捷安特公司的一个项目，主要功能是展示门店数据，产品数据，用户画像，通过折线图，柱状图以及一些可视化工具方便公司管理人员了解公司的运营数据。',
+                '项目主要使用jquery和html结合echarts插件开发，我负责前端所有的技术调研，文档调研等工作',
+                '项目历时两个半月完成第一版上线，上线后获得了捷安特公司的高度认可'
+            ]
+        },
+        {
+            'organization': '骑二无比最萌宝宝投票评选',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2017-03',
+            'endDate': '2017-04',
+            'summary': 'jQuery + html5 + css3 + 原生js',
+            'highlights': [
+                '该项目为公司的赛后最萌宝宝评选功能做的一个项目，完成用户登录，投票，查看投票数据，以及结果查询等功能',
+                '使用微信登录，微信分享等功能。'
+            ]
+        },
+        {
+            'organization': '骑二无比官网',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2017-3',
+            'endDate': '2017-04',
+            'summary': 'jQuery + html5 + css3 + 原生js',
+            'highlights': [
+                '前端基础框架搭建及简单界面设计，基于原生h5开发',
+                '官网的正常展示页等'
+            ]
+        }
+    ],
+    'education': [
+        {
+            'institution': '西安职业技术学院',
+            'area': '计算机应用技术',
+            'studyType': '专科',
+            'startDate': '2011-09',
+            'endDate': '2014-06',
+            // 'gpa': '4.0',
+            'courses': [
+            //     '数据结构',
+            //     '操作系统',
+            //     '编译原理'
+            ]
+        }
+    ],
+    'awards': [
+        // {
+        //     'title': '全国计算机二级证书',
+        //     'date': '2013-11-01',
+        //     'awarder': '教育部考试中心',
+        //     'summary': ''
+        // }
+    ],
+    'publications': [
+
+    ],
+    'skills': [
+        {
+            'name': '前端',
+            'level': 'master',
+            'keywords': [
+                'HTML',
+                'CSS',
+                'Javascript',
+                'Sass',
+                'Gulp',
+                'Webpack',
+                '微信小程序',
+                'Vue',
+                'Vuex',
+                'Axios',
+                'Vue-Router',
+                'Element-UI'
+            ]
+        },
+        {
+            'name': '后端',
+            'level': 'master',
+            'keywords': [
+                'NodeJS',
+                'Express',
+                'mongodb'
+            ]
+        }
+    ],
+    'languages': [
+        {
+            'language': '中文',
+            'fluency': '母语'
+        }
+    ],
+    'interests': [
+        {
+            'name': '社交',
+            'keywords': [
+                '知乎',
+                '微信',
+                'QQ',
+                '头条'
+            ]
+        },
+        {
+            'name': '其他',
+            'keywords': [
+                '中国象棋',
+                '健身',
+                '旅游',
+                '摄影'
+            ]
+        }
+    ],
+    'references': [
+        {
+            'name': '信林子',
+            'reference': '不畏将来，不念过往。'
+        }
+    ]
+}
